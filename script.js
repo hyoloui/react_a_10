023a887b6d7d79c8698e4896c0d84117769d23a7
hodu_Mom <aldud7798555@gmail.com>
2022-11-17 09:18:24 +0900

    test

--- script.js
+++ script.js
@@ -3,26 +3,8 @@ const bts = document.querySelectorAll('.bt1'); // 버튼 전체
 const subbt = document.getElementById('subbt'); // 제출 버튼
 const text = document.getElementById('text'); // input
 const list = document.getElementById('list'); // ul
-
-// 버튼 클릭하면 클릭하는 느낌나게
-
-function handleMouseUp(event) {
-  event.target.classList.add('btclick');
-}
-
-function handleMouseDown(event) {
-  event.target.classList.remove('btclick');
-}
-
-// function handleCreateButton(text) {
-//   completeButton.className = 'btclick bt1';
-//   completeButton.innerText = text;
-// } 튜터님께 줄일 수 있는 방법 물어보기
-
-  // 제출버튼 움직임
-  subbt.addEventListener('mousedown', handleMouseDown);
-  subbt.addEventListener('mouseup', handleMouseUp);
-
+const doneList = document.getElementById('donelist'); // 완료리스트
+const cpbt = document.getElementsByClassName('cpbt'); // 완료버튼
 
 // 엔터로도 리스트 추가 가능
 text.addEventListener('keypress', (e) => {
@@ -39,43 +21,27 @@ const addTodo = () => {
   } else {
     // li 생성 변수
     const temp = document.createElement('li');
-    // button 생성 변수
-    const completeButton = document.createElement('button');
-    const changeButton = document.createElement('button');
-    const deletButton = document.createElement('button');
 
-    // span 생성 변수
-    const createSpan = document.createElement('span');
-
-    // 버튼에 text와 class 넣기
-    completeButton.className = 'btclick bt1';
-    completeButton.innerText = '완료';
-
-    changeButton.className = 'btclick bt1';
-    changeButton.innerText = '수정';
-
-    deletButton.className = 'btclick bt1';
-    deletButton.innerText = '삭제';
-
-    createSpan.innerText = text.value;
-
-    //  버튼 움직임
-    completeButton.addEventListener('mousedown', handleMouseDown);
-    completeButton.addEventListener('mouseup', handleMouseUp);
-
-    changeButton.addEventListener('mousedown', handleMouseDown);
-    changeButton.addEventListener('mouseup', handleMouseUp);
-
-    deletButton.addEventListener('mousedown', handleMouseDown);
-    deletButton.addEventListener('mouseup', handleMouseUp);
-
-    // 추가 완료
-    temp.appendChild(completeButton);
-    temp.appendChild(createSpan);
-    temp.appendChild(changeButton);
-    temp.appendChild(deletButton);
+      temp.innerHTML = `
+    <button class='cpbt' onClick='doneTodo(event)'>완료</button>
+    <span>${text.value}</span>
+    <button class='chbt'>수정</button>
+    <button class='delbt'>삭제</button>
+    `;
 
     list.appendChild(temp);
-    text.value = ''; //리스트 생성 후 input 초기화
+    text.value = ''; //리스트 생성 후 input
   }
 };
+
+// 완료버튼 클릭하면 완료된 목록으로 이동
+function doneTodo(event) {
+  doneList.appendChild(event.target.parentNode);
+  // id donelist에 자식요소로 추가한다. (무엇을)
+  // event가 일어난 target의 부모요소에. 
+  // 여기서 event.target은 cpbt이므로 cpbt의 부모인 li를 뜻한다.
+  cpbt.innerText = '해제';
+  
+}
+
+
