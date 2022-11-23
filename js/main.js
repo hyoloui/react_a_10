2241539d5076ac5025d31005fa797eeb6e09baac
GoMyamMii <sun302700@naver.com>
2022-11-23 14:59:34 +0900

    sign.js 가입 비번 확인 구현 / changsun.html 버그 발생 id 값 변경

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
-import { authService } from './firebase.js';
-import { handleLocation, route } from './router.js';
+import { authService } from "./firebase.js";
+import { handleLocation, route } from "./router.js";
 import {
   handleLogin,
   handleJoin,
   handleConst,
   registerNow,
   login,
-} from './sign.js';
+} from "./sign.js";
 
 // 첫 랜딩 또는 새로고침 시 처리
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
   // 로그인 상태 모니터링
   authService.onAuthStateChanged((user) => {
     // Firebase 연결되면 화면 표시
@@ -18,15 +18,10 @@ document.addEventListener('DOMContentLoaded', function () {
     const hash = window.location.hash;
     if (user) {
       // 로그인 상태이므로 항상 팬명록 화면으로 이동
-      if (hash === '') {
-        // 로그인 상태에서는 로그인 화면으로 되돌아갈 수 없게 설정
-        window.location.replace('#');
-      }
+      console.log("로그인 완료!");
     } else {
       // 로그아웃 상태이므로 로그인 화면으로 강제 이동
-      if (hash !== '') {
-        window.location.replace('');
-      }
+      console.log("로그아웃 상태!");
     }
   });
 });
--- js/sign.js
+++ js/sign.js
@@ -1,5 +1,5 @@
 // import { emailRegex, pwRegex } from "../util.js";
-import { authService } from './firebase.js';
+import { authService } from "./firebase.js";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
@@ -8,30 +8,30 @@ import {
   GithubAuthProvider,
   signOut,
   updateProfile,
-} from 'https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js';
+} from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js";
 
 // 각 아이디 선언
 export function handleConst() {
-  const loginBtn = document.getElementById('loginBtn');
-  const joinBtn = document.getElementById('joinBtn');
-  const memberWrap = document.getElementById('memberWrap');
-  const loginWrap = document.getElementById('loginWrap');
+  const loginBtn = document.getElementById("loginBtn");
+  const joinBtn = document.getElementById("joinBtn");
+  const memberWrap = document.getElementById("memberWrap");
+  const loginWrap = document.getElementById("loginWrap");
 }
 
 // 로그인 버튼 클릭 함수
 export function handleLogin() {
-  loginBtn.classList.add('click');
-  joinBtn.classList.remove('click');
-  memberWrap.classList.add('display');
-  loginWrap.classList.remove('display');
+  loginBtn.classList.add("click");
+  joinBtn.classList.remove("click");
+  memberWrap.classList.add("display");
+  loginWrap.classList.remove("display");
 }
 
 // 회원가입 버튼 클릭 함수
 export function handleJoin() {
-  joinBtn.classList.add('click');
-  loginBtn.classList.remove('click');
-  loginWrap.classList.add('display');
-  memberWrap.classList.remove('display');
+  joinBtn.classList.add("click");
+  loginBtn.classList.remove("click");
+  loginWrap.classList.add("display");
+  memberWrap.classList.remove("display");
 }
 
 // 팬픽 함께하기 누르면 발생하는 함수
@@ -40,19 +40,21 @@ export const registerNow = async (event) => {
   // 이벤트라는 객체가 가지고있는 메소드
   // 앵커태그 등을 클릭하면 자동으로 새로고침시켜주는 것을 막아줌
 
-  const email = document.getElementById('email-new');
+  const email = document.getElementById("email-new");
   const emailVal = email.value;
-  const pw = document.getElementById('pw-new');
+  const pw = document.getElementById("pw-new");
   const pwVal = pw.value;
+  const pwCheck = document.getElementById("pw-check");
+  const pwCheckVal = pwCheck.value;
 
   // 유효성 검사 진행
   if (!emailVal) {
-    alert('이메일을 입력해 주세요');
+    alert("이메일을 입력해 주세요");
     email.focus();
     return;
   }
   if (!pwVal) {
-    alert('비밀번호를 입력해 주세요');
+    alert("비밀번호를 입력해 주세요");
     pw.focus();
     return;
   }
@@ -74,44 +76,51 @@ export const registerNow = async (event) => {
   // 유효성 검사 통과 후 로그인 또는 회원가입 API 요청
 
   // 회원가입 버튼 클릭의 경우
-  await createUserWithEmailAndPassword(authService, emailVal, pwVal)
-    .then(() => {
-      // Signed in
-      console.log('회원가입 성공!');
-      alert('축하합니다. 회원이 되셨습니다.');
+  if (pwVal === pwCheckVal) {
+    await createUserWithEmailAndPassword(authService, emailVal, pwVal)
+      .then(() => {
+        // Signed in
+        console.log("회원가입 성공!");
+        alert("축하합니다. 회원이 되셨습니다.");
 
-      // 닉네임을 받아서 추가시켜줌
-      updateProfile(authService.currentUser, {
-        displayName: document.getElementById('name-new').value,
+        // 닉네임을 받아서 추가시켜줌
+        updateProfile(authService.currentUser, {
+          displayName: document.getElementById("name-new").value,
+        });
+      })
+      .catch((error) => {
+        const errorMessage = error.message;
+        console.log("errorMessage:", errorMessage);
+        if (errorMessage.includes("email-already-in-use")) {
+          alert("이미 가입된 이메일입니다.");
+        }
       });
-    })
-    .catch((error) => {
-      const errorMessage = error.message;
-      console.log('errorMessage:', errorMessage);
-      if (errorMessage.includes('email-already-in-use')) {
-        alert('이미 가입된 이메일입니다.');
-      }
-    });
+  } else {
+    alert("비밀번호를 확인해주세요");
+  }
 };
 
 // update pofile -> 닉네임 바꾸기
 // async
 
 // 로그인 함수
-export const login = () => {
-  const email = document.getElementById('email');
+export const login = (event) => {
+  event.preventDefault();
+  console.log("로그인을 눌렀어요");
+
+  const email = document.getElementById("email");
   const emailVal = email.value;
-  const pw = document.getElementById('pw');
+  const pw = document.getElementById("pw");
   const pwVal = pw.value;
 
   // 유효성 검사 진행
   if (!emailVal) {
-    alert('이메일을 입력해 주세요');
+    alert("이메일을 입력해 주세요");
     email.focus();
     return;
   }
   if (!pwVal) {
-    alert('비밀번호를 입력해 주세요');
+    alert("비밀번호를 입력해 주세요");
     pw.focus();
     return;
   }
@@ -119,19 +128,18 @@ export const login = () => {
   signInWithEmailAndPassword(authService, emailVal, pwVal)
     .then((userCredential) => {
       // Signed in
-      console.log("로그인 성공! 로그인 정보", userCredential)
+      console.log("로그인 성공! 로그인 정보", userCredential);
       const user = userCredential.user;
-      window.location.hash = '';
+      window.location.hash = "";
     })
     .catch((error) => {
       const errorMessage = error.message;
-      console.log('errorMessage:', errorMessage);
-      if (errorMessage.includes('user-not-found')) {
-        alert('가입되지 않은 회원입니다.');
+      console.log("errorMessage:", errorMessage);
+      if (errorMessage.includes("user-not-found")) {
+        alert("가입되지 않은 회원입니다.");
         return;
-      } else if (errorMessage.includes('wrong-password')) {
-        alert('비밀번호가 잘못 되었습니다.');
+      } else if (errorMessage.includes("wrong-password")) {
+        alert("비밀번호가 잘못 되었습니다.");
       }
     });
 };
-
