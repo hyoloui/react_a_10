1d9433a72bb8c59ae3aa0daf3f5892fc9245a917
GoMyamMii <sun302700@naver.com>
2022-11-24 14:13:27 +0900

    add 안된거 추가

--- js/firebase.js
+++ js/firebase.js
@@ -1,17 +1,17 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js';
-import { getAuth } from 'https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js';
-import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js';
-import { getStorage } from 'https://www.gstatic.com/firebasejs/9.14.0/firebase-storage.js';
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-storage.js";
 
 // 아래 데이터는 본인의 Firebase 프로젝트 설정에서 확인할 수 있습니다.
 const firebaseConfig = {
-  apiKey: 'AIzaSyCfcdAPgNz4yL353KJuMHzt0KCEtjxuyQo',
-  authDomain: 'codingapple-1d8ee.firebaseapp.com',
-  projectId: 'codingapple-1d8ee',
-  storageBucket: 'codingapple-1d8ee.appspot.com',
-  messagingSenderId: '77306771818',
-  appId: '1:77306771818:web:c286e15a443b1b05f00d13',
+  apiKey: "AIzaSyCnDFHfwwiIhl6RcDA-qeQepCo5HM5_WyE",
+  authDomain: "teampro02-ef6d8.firebaseapp.com",
+  projectId: "teampro02-ef6d8",
+  storageBucket: "teampro02-ef6d8.appspot.com",
+  messagingSenderId: "919485897264",
+  appId: "1:919485897264:web:a7f576274dde722d3d2bf5",
 };
 
 // Initialize Firebase
